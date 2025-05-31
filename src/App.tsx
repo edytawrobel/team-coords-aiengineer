ae223b7774e70ff2c635ecf6488905b9547ed609
edytawrobel <edyta.wrobel.willdoit@gmail.com>
2025-05-30 23:26:07 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             <Route path="/schedule" element={<Schedule />} />
             <Route path="/add-session" element={<AddSession />} />
             <Route path="/notes" element={<NotesExchange />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="*" element={<Navigate to="/\" replace />} />
           </Routes>
         </div>
       </Router>
